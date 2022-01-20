07a988e09e6c600d365c249fea697036ff11b7fc
IBenCubing <i.ben.cubing@gmail.com>
2022-01-20 00:49:20 -0600

    enable autologin

--- root.sh
+++ root.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-username = $(id -u -n 1000)
+username=$(id -u -n 1000)
 
 apt update
 apt install xorg bspwm sxhkd rofi kitty neofetch -y
@@ -7,3 +7,14 @@ apt install xorg bspwm sxhkd rofi kitty neofetch -y
 mkdir -p /home/$username/.config
 cp -R .config/* /home/$username/.config/
 chown -R $username:$username /home/$username
+
+mkdir -p /etc/systemd/system/getty@tty1.service.d/
+echo '[Service]
+ExecStart=
+ExecStart=-/sbin/agetty -a '$username' %I $TERM' > /etc/systemd/system/getty@tty1.service.d/override.conf
+
+cp /home/$username/.profile /home/$username/.profile.bak
+echo '[ "$(tty)" = "/dev/tty1" ] && exec startx' >> /home/$username/.profile
+cat /home/$username/.profile
+
+systemctl set-default multi-user.target
